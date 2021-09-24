367d7f9e1601e09f487d9dcf8eea4bd77bac2995
Meruzhan <90762301+nepaleb0@users.noreply.github.com>
2021-09-24 10:39:50 +0300

    Add files via upload

---
+++ lab5/2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout <<"Введите 3 точки: ";
+	int a, b, c;
+	cin >>a>>b>>c;
+	int ac, bc;
+	ac = c-a;
+	bc = c-b;
+	cout <<"AC равно: "<<ac<<endl
+		<<"BC равно: "<<bc;
+}
---
+++ lab5/3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a,b,c;
+	cout <<"Введите 3 точки: ";
+	cin >>a>>b>>c;
+	int ac, bc;
+	if (c > a){
+		ac = c - a;
+		bc = b - c;
+	}else{
+		ac = a - c;
+		bc = c - b;
+	}
+	cout << "AC равно: "<<ac<<endl
+		<<"BC равно: "<<bc;
+}
