96fb66a13f8149f8b74d5e9e46bc4aead0335669
Meruzhan <90762301+nepaleb0@users.noreply.github.com>
2021-09-29 13:26:56 +0300

    Update 6.cpp

--- lab7/6.cpp
+++ lab7/6.cpp
@@ -3,11 +3,22 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	cout <<"A1*x + B1*y = C1"<<endl
-		<<"A2*x + B1*y = C2"<<endl
-		<<"Введите коэфиценты A, B и С для двух уравнений";
-	int a1,b1,c1,a2,b2,c3;
-	cin >>a>>b>>c;
-	cout <<"x = "<<(c2-(b2*c1))/((b1*a2)-a1)<<endl		//ax+by=c --> y=(c-ax)/b
-		<<"y = "<<(c1-(a1*x))/b1
+	cout << "A1*x + B1*y = C1" << endl;
+	cout << "A2*x + B1*y = C2" << endl;
+	cout << "Введите коэфиценты A, B и С для двух уравнений ";
+	float a1, a2, b1, b2, c1, c2, c, x, y;
+	cin >> a1 >> b1 >> c1;
+	cin >> a2 >> b2 >> c2;
+	c = a1 * b2 - b1 * a2;
+	x = c1 * b2 - b1 * c2;
+	y = a1 * c2 - a2 * c1;
+	if (c == 0){
+	    return 0;
+	}else{
+    	x = x/c;
+    	y = y/c;
+    	cout << "x = " << x;
+    	cout << "\ny = " << y;
+
+	}	
 }
